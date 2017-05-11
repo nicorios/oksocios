df6cb87fad6d0689f3582c78dae457ad3d406f64
Nicolas Rios <riosc.nicolas@gmail.com>
2017-05-10 23:33:29 -0300

    Initial commit

---
+++ src/main/java/com/oksocios/OksociosApiApplication.java
@@ -0,0 +1,12 @@
+package com.oksocios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OksociosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OksociosApiApplication.class, args);
+	}
+}
