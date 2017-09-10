1cb532083a736765d6149d0dd71162b9e943c8cd
Nico <riosc.nicolas@gmail.com>
2017-09-10 03:36:08 -0300

    Concepts Settings View

--- src/main/java/com/oksocios/controller/SettingsController.java
+++ src/main/java/com/oksocios/controller/SettingsController.java
@@ -1,7 +1,9 @@
 package com.oksocios.controller;
 
 import com.oksocios.model.Activity;
+import com.oksocios.model.Concept;
 import com.oksocios.service.ActivityService;
+import com.oksocios.service.ConceptService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,16 +15,21 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 public class SettingsController {
 
     private final ActivityService activityService;
+    private final ConceptService conceptService;
+
 
     @Autowired
-    public SettingsController(ActivityService activityService){
+    public SettingsController(ActivityService activityService, ConceptService conceptService){
         this.activityService = activityService;
+        this.conceptService = conceptService;
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/settings")
     public String getSettings(Model model, @SessionAttribute Long idEstablishment){
         model.addAttribute("activities", activityService.getAllActivitiesByEstablishment(idEstablishment));
         model.addAttribute("activity", new Activity());
+        model.addAttribute("concepts", conceptService.getConceptsByEstablishmentId(idEstablishment));
+        model.addAttribute("concept", new Concept());
         return "settings";
     }
 
