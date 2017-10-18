175822be09c1c79cf83548c694f5d0a2be17aa03
Nico <riosc.nicolas@gmail.com>
2017-10-17 22:28:01 -0300

    Email sending async

--- src/main/java/com/oksocios/service/EmailService.java
+++ src/main/java/com/oksocios/service/EmailService.java
@@ -4,6 +4,7 @@ package com.oksocios.service;
 import com.oksocios.model.Email;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +16,7 @@ public class EmailService {
         this.javaMailSender = javaMailSender;
     }
 
+    @Async
     public void sendMail(Email email) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(email.getEmailTo());
