92e7c01368f9620f4621f2160ee362a5b9a63e8a
Nico <riosc.nicolas@gmail.com>
2017-11-11 14:14:29 -0300

    Run app

--- scripts/deploy_service.sh
+++ scripts/deploy_service.sh
@@ -1,1 +1,8 @@
-pwd
+#kill previous process
+kill -9 $(ps -aux | grep 'java -jar' | grep 'oksocios-api' | awk '{print $2}')
+
+#Wait a little
+sleep 2
+
+#Start oksocios
+(nohup java -jar "/home/ubuntu/src-oksocios/target/oksocios-api-1.0.0.jar")
