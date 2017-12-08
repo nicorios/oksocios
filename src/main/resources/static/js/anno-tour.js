edf13daf26d540df945d05172a119a1969a3f1f3
Nico <riosc.nicolas@gmail.com>
2017-12-08 00:09:10 -0300

    Demo in the rest of the system

--- src/main/resources/static/js/anno-tour.js
+++ src/main/resources/static/js/anno-tour.js
@@ -36,7 +36,7 @@ let annoTour = function(name){
           let anno_new_entry = new Anno([{
                   target  : '#login-page',
                   position: 'right',
-                  content : "Aqui puedes cargar un nuevo ingreso al establecimiento con el dni de un socio. El sistema verificará que dicho socio tenga una suscripción válida"
+                  content : "Aqui puedes cargar un nuevo ingreso al establecimiento con el dni de un socio. El sistema verificará que dicho socio tenga una suscripción válida. (Puedes intentarlo con el DNI 12345678)"
               }]);
 
           setTimeout(function(){
@@ -110,6 +110,90 @@ let annoTour = function(name){
 
           setTimeout(function(){
               anno_balance.show();
+          }, 2000);
+          break;
+      case 'staff':
+          let anno_staff = new Anno([{
+                  target  : '#tbl-admins',
+                  position: 'center-bottom',
+                  content : "En esta tabla se detallan los administradores del establecimiento. Un administrador es el único capaz de editar o eliminar su cuenta."
+              },
+              {
+                  target  : '#tbl-employees',
+                  position: 'center-top',
+                  content : "En ésta tabla se muestran los empleados. Un administrador puede tanto editar como eliminar registros de empleados."
+              },
+              {
+                  target  : '#div-new-staff',
+                  position: 'center-top',
+                  content : "Aquí puedes cargar administradores y empleados del establecimiento. Los Empleados tienen un nivel de acceso menor, puedes crear uno y luego loguearte para comprobarlo."
+              }]);
+
+          setTimeout(function(){
+              anno_staff.show();
+          }, 2000);
+          break;
+      case 'settings':
+          let anno_settings = new Anno([{
+                  target  : '#section-activities',
+                  position: 'center-bottom',
+                  content : "Aquí puedes ver las Actividades disponibles para que los socios se suscriban en tu establecimiento. Puedes crear nuevas, editar o eliminar las existentes."
+              },
+              {
+                  target  : '#section-concepts',
+                  position: 'center-top',
+                  content : "Mientras que aquí puedes ver los Conceptos que son los que tienen un gasto/ingreso asociado en la pestaña Caja. Puedes agregar nuevos, editar o eliminar los existentes."
+              }]);
+
+          setTimeout(function(){
+              anno_settings.show();
+          }, 2000);
+          break;
+      case 'new-customer':
+          let anno_new_customer = new Anno([{
+                  target  : '#div-new-customer',
+                  position: 'center-right',
+                  content : "En éste formulario puedes cargar un nuevo socio en tu establecimiento."
+              },
+              {
+                  target  : '#div-new-subscription',
+                  position: 'center-left',
+                  content : "Mientras que aquí puedes cargar una nueva suscripción a un socio a partir de su DNI, previamente debes haber cargado al menos una Actividad en la pestaña Ajustes."
+              }]);
+
+          setTimeout(function(){
+              anno_new_customer.show();
+          }, 2000);
+          break;
+      case 'customer-profile':
+          let anno_customer_profile = new Anno([{
+                  target  : '#profile-page-header',
+                  position: 'center-bottom',
+                  content : "En esta sección puedes ver la información básica del socio."
+              },
+              {
+                  target  : '#profile-subscription',
+                  position: 'center-right',
+                  content : "Aquí puedes ver un detalle del tipo de suscripción que tiene el socio actualmente."
+              },
+              {
+                  target  : '#profile-location',
+                  position: 'center-right',
+                  content : "Mientras que aquí un pequeño mapa que detalla su domicilio."
+              },
+              {
+                  target  : '#profile-page-wall-share',
+                  position: 'center-left',
+                  content : "En ésta tabla se muestra un detalle de los ingresos al establecimiento del socio."
+              },
+              {
+                  target  : '#profile-page-wall-posts',
+                  position: 'center-left',
+                  content : "Por último, en este campo se puede enviar un correo electrónico que el socio recibira en su casilla de correo."
+              }]);
+
+          setTimeout(function(){
+              anno_customer_profile.show();
           }, 2000);
           break;
   }
