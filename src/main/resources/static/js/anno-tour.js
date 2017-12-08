41a367dc93806b097cb70ca5608ba823d6f51ae1
Nico <riosc.nicolas@gmail.com>
2017-12-07 21:28:34 -0300

    Progress in demo

--- src/main/resources/static/js/anno-tour.js
+++ src/main/resources/static/js/anno-tour.js
@@ -1,7 +1,7 @@
-var annoTour = function(name){
+let annoTour = function(name){
   switch (name){
       case 'home':
-          var anno_home = new Anno([{
+          let anno_home = new Anno([{
                   target  : '#left-sidebar-nav',
                   position: 'right',
                   content : "Nos encontramos en la pantalla Principal, puedes acceder a las diferentes pantallas del sistema mediante los siguientes links."
@@ -22,24 +22,52 @@ var annoTour = function(name){
           }, 2000);
           break;
       case 'customers':
-          var anno_subscriptions = new Anno([{
-                  target  : '#left-sidebar-nav',
-                  position: 'right',
-                  content : "Nos encontramos en la pantalla Principal, puedes acceder a las diferentes pantallas del sistema mediante los siguientes links."
-              },
-              {
+          let anno_subscriptions = new Anno([{
                   target  : '#tbl-subscriptions',
                   position: 'center-bottom',
-                  content : "Aquí puedes ver los socios que tienes registrados en tu establecimiento."
-              },
-              {
-                  target  : '#fab',
-                  position: 'left',
-                  content : "Con el botón rojo puedes cargar el ingreso de un nuevo socio al establecimiento mientras que con el amarillo una nueva suscripción."
+                  content : "Aquí puedes ver las suscripciones actuales en tu establecimiento."
               }]);
 
           setTimeout(function(){
               anno_subscriptions.show();
+          }, 2000);
+          break;
+      case 'new-entry':
+          let anno_new_entry = new Anno([{
+                  target  : '#login-page',
+                  position: 'right',
+                  content : "Aqui puedes cargar un nuevo ingreso al establecimiento con el dni de un socio. El sistema verificará que dicho socio tenga una suscripción válida"
+              }]);
+
+          setTimeout(function(){
+              anno_new_entry.show();
+          }, 2000);
+          break;
+      case 'activities-stats':
+          let anno_stats_activities = new Anno([{
+                  target  : '#stats-activities',
+                  position: 'left',
+                  content : "Este gráfico muestra las suscripciones registradas durante el último año."
+              },
+              {
+                  target  : '#select-activity',
+                  position: 'center-bottom',
+                  content : "Puedes filtrar por actividad."
+              }]);
+
+          setTimeout(function(){
+              anno_stats_activities.show();
+          }, 2000);
+          break;
+      case 'customers-stats':
+          let anno_stats_customers = new Anno([{
+                  target  : '#stats-customers',
+                  position: 'left',
+                  content : "Este gráfico muestra las de los socios del establecimiento."
+              }]);
+
+          setTimeout(function(){
+              anno_stats_customers.show();
           }, 2000);
           break;
   }
